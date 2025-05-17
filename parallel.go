3961e8ff344ee7bb13113363c983e17c2d337970
Frank Bagherzadeh <frankbzd@gmail.com>
2025-05-17 00:04:33 -0400

    no comment zone

--- parallel.go
+++ parallel.go
@@ -5,36 +5,35 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"go.uber.org/zap"
 )
 
-// Process is an interface for components that can be started and stopped.
 type Process interface {
 	Run(ctx context.Context) error
-	Stop(ctx context.Context) error
+	Stop()
 	Name() string
 }
 
-// Conductor manages the lifecycle of multiple Process components.
-type Conductor struct {
-	log       *zap.Logger
-	stop      chan os.Signal
-	processes []Process
-	ctx       context.Context
-	cancel    context.CancelFunc
+type processError struct {
+	process Process
+	err     error
 }
 
-// NewConductor initializes a new Conductor with provided processes and sets up signal handling.
-func NewConductor(ctx context.Context, processes ...Process) *Conductor {
-	ctx, cancel := context.WithCancel(ctx)
+type Conductor struct {
+	// TODO: add a logger interface
+	log       *zap.Logger
+	stop      chan os.Signal
+	errors    chan processError
+	processes []Process
+}
+
+func NewConductor(processes ...Process) *Conductor {
 	r := &Conductor{
 		log:       zap.NewExample(),
-		stop:      make(chan os.Signal, 1),
+		stop:      make(chan os.Signal),
+		errors:    make(chan processError, len(processes)),
 		processes: processes,
-		ctx:       ctx,
-		cancel:    cancel,
 	}
 
 	signal.Notify(r.stop, os.Interrupt, syscall.SIGTERM)
@@ -42,41 +41,48 @@ func NewConductor(ctx context.Context, processes ...Process) *Conductor {
 	return r
 }
 
-// Run starts all processes in separate goroutines.
-func (r *Conductor) Run() *Conductor {
-	for _, process := range r.processes {
-		go func(p Process) {
-			r.log.Info("running", zap.String("process", p.Name()))
-			if err := p.Run(r.ctx); err != nil {
-				r.log.Panic("failed to run", zap.String("process", p.Name()), zap.Error(err))
+func (c *Conductor) Run(ctx context.Context) *Conductor {
+	go c.monitor(ctx)
+
+	for _, p := range c.processes {
+		go func(process Process) {
+			c.log.Info("running", zap.String("process", process.Name()))
+
+			if err := process.Run(ctx); err != nil {
+				c.errors <- processError{
+					process: process,
+					err:     err,
+				}
+
+				return
 			}
 
-			r.log.Info("ran", zap.String("process", p.Name()))
-		}(process)
+			c.log.Info("successfully ran", zap.String("process", process.Name()))
+		}(p)
 	}
 
-	return r
+	return c
 }
 
-// Stop stops all processes gracefully within a 5-second timeout.
-func (r *Conductor) Stop() {
-	ctx, cancel := context.WithTimeout(r.ctx, 5*time.Second)
-	defer cancel()
+func (c *Conductor) ThenStop() {
+	<-c.stop
+	c.log.Info("received stop signal, stopping all processes")
 
-	for _, process := range r.processes {
-		r.log.Info("stopping", zap.String("process", process.Name()))
-		if err := process.Stop(ctx); err != nil {
-			r.log.Panic("failed to stop", zap.String("process", process.Name()), zap.Error(err))
-		}
-
-		r.log.Info("stopped", zap.String("process", process.Name()))
+	for _, process := range c.processes {
+		process.Stop()
+		c.log.Info("stopped", zap.String("process", process.Name()))
 	}
 
-	r.cancel()
+	close(c.stop)
 }
 
-// ThenStop waits for an OS signal and then stops all processes.
-func (r *Conductor) ThenStop() {
-	<-r.stop
-	r.Stop()
+func (c *Conductor) monitor(ctx context.Context) {
+	select {
+	case err := <-c.errors:
+		c.log.Error("error occurred", zap.String("process", err.process.Name()), zap.Error(err.err))
+		c.stop <- syscall.SIGTERM
+	case <-ctx.Done():
+		c.log.Warn("context canceled")
+		c.stop <- syscall.SIGTERM
+	}
 }
